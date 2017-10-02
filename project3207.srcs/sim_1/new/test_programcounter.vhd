f9eb577a0c61ddfbfd1c091c82a8486357afe9a0
DESKTOP-GSUL1UG\Sneha Prabakar <sneha.prabakar@u.nus.edu>
2017-10-02 23:29:43 +0800

    Test case 3 added + suggested changes made for spacing

--- project3207.srcs/sim_1/new/test_programcounter.vhd
+++ project3207.srcs/sim_1/new/test_programcounter.vhd
@@ -39,11 +39,12 @@ end test_programcounter;
 architecture test_programcounter_behavioral of test_programcounter is
 
     component programcounter
-        port ( CLK   : in std_logic;
-               RESET : in std_logic;
-               WE_PC : in std_logic;
-               PC_IN : in std_logic_vector (31 downto 0);
-               PC    : out std_logic_vector (31 downto 0) := (others => '0'));
+    port ( 
+        CLK   : in std_logic;
+        RESET : in std_logic;
+        WE_PC : in std_logic;
+        PC_IN : in std_logic_vector (31 downto 0);
+        PC    : out std_logic_vector (31 downto 0) := (others => '0'));
     end component;
 
     signal t_CLK   : std_logic;
@@ -70,13 +71,15 @@ begin
     -- Clock generation
     clk_process: process begin
         t_CLK <= '1';
-        wait for ClkPeriod / 2;  --for 0.5 ns signal is '1'.
+        wait for ClkPeriod / 2;  -- for 0.5 ns signal is '1'.
         t_CLK <= '0';
-        wait for ClkPeriod / 2;  --for next 0.5 ns signal is '0'.
+        wait for ClkPeriod / 2;  -- for next 0.5 ns signal is '0'.
     end process;
 
-    stim_proc : process begin
+    stim_proc: process begin
         -- Set initial value for inputs
+        -- Test case 1: RESET = 1, WE_PC = 0
+        -- So the PC should be x"00000000"
         t_RESET <= '0'; t_WE_PC <= '0'; t_PC_IN <= (others => '0');
         -- Inputs will be changed and checked between clock edges to avoid indeterminate behaviour at the edge
         -- Each test case will start at x.5 ns, where x is 0, 1, 2... This is to keep track of where the clock is
@@ -85,20 +88,25 @@ begin
                
         -- Before time = ClkPeriod, some signals may be U or X. That is expected, as the processor is only reset
         -- at the first clock edge, and this is when the PC is set to 0. Before this, PC is indeterminate.
-               
-        -- RESET = 1 to set PC to 0
-        t_RESET <= '1'; 
+              
+        t_RESET <= '1'; t_PC_IN <= x"00000004";
         wait for ClkPeriod;
         
-        -- Test case 1: RESET = 1, WE_PC = 0
         -- PC_IN = x"00000000"
         assert (t_PC = x"00000000") report "Failed Program Counter Test Case 1" severity error;
        
         -- Test case 2: RESET = 0, WE_PC = 1
+        -- So the PC should be equal to PC_IN
         -- PC_IN = x"00000004"
-        t_RESET <= '0'; t_WE_PC <= '1'; t_PC_IN <= x"00000004";  
-        wait for ClkPeriod * 9 / 10;
-        assert (t_PC = x"00000004") report "Failed Program Counter Test Case 2" severity error;
+        t_RESET <= '0'; t_WE_PC <= '1'; t_PC_IN <= x"00000008";  
+        wait for ClkPeriod;
+        assert (t_PC = x"00000008") report "Failed Program Counter Test Case 2" severity error;
+        
+        -- Test case 3: RESET = 0, WE_PC = 0
+        -- So the PC should not get updated (i.e. affected by the new PC_IN)
+        t_RESET <= '0'; t_WE_PC <= '0'; t_PC_IN <= x"0000000c";
+        wait for ClkPeriod;
+        assert (t_PC = x"00000008") report "Failed Program Counter Test Case 3" severity error;
    
         wait;
         
