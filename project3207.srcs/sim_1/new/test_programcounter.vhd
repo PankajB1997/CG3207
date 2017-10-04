ea87d125a5ce3abc834c31826f250a23e79529d3
DESKTOP-GSUL1UG\Sneha Prabakar <sneha.prabakar@u.nus.edu>
2017-10-04 19:33:54 +0800

    Useless comments removed

--- project3207.srcs/sim_1/new/test_programcounter.vhd
+++ project3207.srcs/sim_1/new/test_programcounter.vhd
@@ -1,35 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 10/02/2017 09:25:30 PM
--- Design Name: 
--- Module Name: test_programcounter - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity test_programcounter is
 --  Port ( );
