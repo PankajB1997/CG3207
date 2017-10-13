f2ffd7bc5b995322a8d3bef36c0dedad9bc7f0f6
LAPTOP-HFGK7OGE\pankaj <pankajbhootra@gmail.com>
2017-10-13 12:04:10 +0800

    Unsigned mul with one adder in MCycle

--- project3207.srcs/sources_1/imports/Lab_2_VHDL/MCycle.vhd
+++ project3207.srcs/sources_1/imports/Lab_2_VHDL/MCycle.vhd
@@ -95,7 +95,7 @@ begin
 
     computing_process : process (CLK) -- process which does the actual computation
     variable count : std_logic_vector(7 downto 0) := (others => '0'); -- assuming no computation takes more than 256 cycles.
-    variable temp_sum : std_logic_vector(2 * width - 1 downto 0) := (others => '0');
+    variable temp : std_logic_vector(2 * width - 1 downto 0) := (others => '0');
     variable shifted_op1 : std_logic_vector(2 * width - 1 downto 0) := (others => '0');
     variable shifted_op2 : std_logic_vector(2 * width - 1 downto 0) := (others => '0');
     variable shifted_dividend : std_logic_vector(2 * width downto 0) := (others => '0');
@@ -106,7 +106,7 @@ begin
             -- n_state = COMPUTING and state = IDLE implies we are just transitioning into COMPUTING
             if RESET = '1' or (n_state = COMPUTING and state = IDLE) then
                 count := (others => '0');
-                temp_sum := (others => '0');
+                temp := (others => '0');
                 shifted_op1 := (2 * width - 1 downto width => not(MCycleOp(0)) and Operand1(width - 1)) & Operand1;
                 shifted_op2 := (2 * width - 1 downto width => not(MCycleOp(0)) and Operand2(width - 1)) & Operand2;
                 shifted_dividend := (2 * width downto width + 1 => '0') & Operand1 & '0';
@@ -116,15 +116,21 @@ begin
             done <= '0';
 
             if MCycleOp(1) = '0' then -- Multiply
-                -- MCycleOp(0) = '0' takes 2 * 'width' cycles to execute, returns signed(Operand1) * signed(Operand2)
-                -- MCycleOp(0) = '1' takes 'width' cycles to execute, returns unsigned(Operand1) * unsigned(Operand2)
-                if shifted_op2(0) = '1' then -- add only if b0 = 1
-                    temp_sum := temp_sum + shifted_op1;
+                -- MCycleOp(0) = '0' takes ??? cycles to execute, returns signed(Operand1) * signed(Operand2)
+                -- MCycleOp(0) = '1' takes 'width + 1' cycles to execute, returns unsigned(Operand1) * unsigned(Operand2)
+                if count /= 0 then
+                  temp := sum & temp(width - 1 downto 1);
+                  shifted_op2 := '0' & shifted_op2(2 * width - 1 downto 1);
                 end if;
-                shifted_op2 := '0'& shifted_op2(2 * width - 1 downto 1);
-                shifted_op1 := shifted_op1(2 * width - 2 downto 0) & '0';
-                Result2 <= temp_sum(2 * width - 1 downto width);
-                Result1 <= temp_sum(width - 1 downto 0);
+                Result2 <= temp(2 * width - 1 downto width);
+                Result1 <= temp(width - 1 downto 0);
+                srcA <= '0' & temp(2 * width - 1 downto width);
+                if shifted_op2(0) = '1' then -- add only if b0 = 1
+                  srcB <= '0' & Operand1;
+                else
+                  srcB <= (others => '0');
+                end if;
+                cIn <= (others => '0');
             else -- Divide
                 -- MCycleOp(0) = '0' takes ??? cycles to execute, returns signed(Operand1)/signed(Operand2)
                 -- MCycleOp(0) = '1' takes 'width' cycles to execute, returns unsigned(Operand1)/unsigned(Operand2)
@@ -144,8 +150,8 @@ begin
             end if;
             -- regardless of multiplication or division, check if last cycle is reached
             -- right now, below assumes that signed division takes (2 * width) cycles, may need to change
-            if (MCycleOp = "00" and count = 2 * width - 1) or
-               (MCycleOp = "01" and count = width - 1) or
+            if (MCycleOp = "00" and count = 2 * width) or
+               (MCycleOp = "01" and count = width) or
                (MCycleOp(1) = '1' and count = width) then     -- If last cycle
                 done <= '1';
             end if;
