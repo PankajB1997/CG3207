2b2440add53a0cfcd1c58d13081f96213b435417
LAPTOP-HFGK7OGE\pankaj <pankajbhootra@gmail.com>
2017-11-07 21:57:54 +0800

    Update conditional logic tests

--- project3207.srcs/sim_1/new/test_condlogic.vhd
+++ project3207.srcs/sim_1/new/test_condlogic.vhd
@@ -87,16 +87,16 @@ begin
         wait for ClkPeriod / 2;
 
         -- Test case 1: For 'always' condition, incoming true signals are transferred out 'immediately'.
-        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_Cond <= "1110";
+        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_MCycleS <= '1'; t_InterruptControlW <= '1'; t_Cond <= "1110";
         wait for ClkPeriod / 10;
-        assert (t_PCSrc = '1' and t_RegWrite = '1' and t_MemWrite = '1' and t_CarryFlag = '0') report "Failed CondLogic Test Case 1" severity error;
+        assert (t_PCSrc = '1' and t_RegWrite = '1' and t_MemWrite = '1' and t_CarryFlag = '0' and t_InterruptControlWrite = '1' and t_MCycleStart = '1') report "Failed CondLogic Test Case 1" severity error;
 
         wait for ClkPeriod * 9 / 10;
 
         -- Test case 2: For 'always' condition, incoming false signals are transferred out 'immediately'.
-        t_PCS <= '0'; t_RegW <= '0'; t_MemW <= '0'; t_Cond <= "1110";
+        t_PCS <= '0'; t_RegW <= '0'; t_MemW <= '0'; t_MCycleS <= '0'; t_InterruptControlW <= '0'; t_Cond <= "1110";
         wait for ClkPeriod / 10;
-        assert (t_PCSrc = '0' and t_RegWrite = '0' and t_MemWrite = '0' and t_CarryFlag = '0') report "Failed CondLogic Test Case 2" severity error;
+        assert (t_PCSrc = '0' and t_RegWrite = '0' and t_MemWrite = '0' and t_CarryFlag = '0' and t_InterruptControlWrite = '0' and t_MCycleStart = '0') report "Failed CondLogic Test Case 2" severity error;
 
         wait for ClkPeriod * 9 / 10;
 
@@ -110,17 +110,17 @@ begin
 
         -- Test case 4: For some false condition, signals are all false.
         -- Flags initialised to false, so EQ condition will be false (Z != 1).
-        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_Cond <= "0000";
+        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_MCycleS <= '1'; t_InterruptControlW <= '1'; t_Cond <= "0000";
         wait for ClkPeriod / 10;
-        assert (t_PCSrc = '0' and t_RegWrite = '0' and t_MemWrite = '0' and t_CarryFlag = '0') report "Failed CondLogic Test Case 4" severity error;
+        assert (t_PCSrc = '0' and t_RegWrite = '0' and t_MemWrite = '0' and t_CarryFlag = '0' and t_InterruptControlWrite = '0' and t_MCycleStart = '0') report "Failed CondLogic Test Case 4" severity error;
 
         wait for ClkPeriod * 9 / 10;
 
         -- Test case 5: For some true condition, signals are transferred correctly.
         -- Flags initialised to false, so NEQ condition will be true (Z == 0).
-        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_Cond <= "0001";
+        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_MCycleS <= '0'; t_InterruptControlW <= '0'; t_Cond <= "0001";
         wait for ClkPeriod / 10;
-        assert (t_PCSrc = '1' and t_RegWrite = '1' and t_MemWrite = '1' and t_CarryFlag = '0') report "Failed CondLogic Test Case 5" severity error;
+        assert (t_PCSrc = '1' and t_RegWrite = '1' and t_MemWrite = '1' and t_CarryFlag = '0' and t_InterruptControlWrite = '0' and t_MCycleStart = '0') report "Failed CondLogic Test Case 5" severity error;
 
         wait for ClkPeriod * 9 / 10;
 
@@ -130,14 +130,14 @@ begin
 
         -- Test case 6: Assert flags have changed state by checking signals have transferred correctly depending on condition.
         -- Flags are all true, so EQ condition will be true (Z == 1);
-        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_Cond <= "0000";
+        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '0'; t_MCycleS <= '0'; t_InterruptControlW <= '0'; t_Cond <= "0000";
         wait for ClkPeriod / 10;
-        assert (t_PCSrc = '1' and t_RegWrite = '1' and t_MemWrite = '1' and t_CarryFlag = '1') report "Failed CondLogic Test Case 6.1" severity error;
+        assert (t_PCSrc = '1' and t_RegWrite = '1' and t_MemWrite = '0' and t_CarryFlag = '1' and t_InterruptControlWrite = '0' and t_MCycleStart = '0') report "Failed CondLogic Test Case 6.1" severity error;
         wait for ClkPeriod / 10;
         -- Flags are all true, so NEQ condition will be false (Z != 0)
-        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_Cond <= "0001";
+        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_MCycleS <= '1'; t_InterruptControlW <= '1'; t_Cond <= "0001";
         wait for ClkPeriod / 10;
-        assert (t_PCSrc = '0' and t_RegWrite = '0' and t_MemWrite = '0' and t_CarryFlag = '1') report "Failed CondLogic Test Case 6.2" severity error;
+        assert (t_PCSrc = '0' and t_RegWrite = '0' and t_MemWrite = '0' and t_CarryFlag = '1' and t_InterruptControlWrite = '1' and t_MCycleStart = '1') report "Failed CondLogic Test Case 6.2" severity error;
 
         wait for ClkPeriod * 7 / 10;
 
@@ -146,12 +146,12 @@ begin
         t_FinalFlags <= "0000"; t_FlagW <= "111";
         wait for ClkPeriod / 10;
         -- Flags should still be true, so EQ will be true (Z == 1).
-        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_Cond <= "0000";
+        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_MCycleS <= '1'; t_InterruptControlW <= '1'; t_Cond <= "0000";
         wait for ClkPeriod / 10;
-        assert (t_PCSrc = '1' and t_RegWrite = '1' and t_MemWrite = '1' and t_CarryFlag = '1') report "Failed CondLogic Test Case 7.1" severity error;
+        assert (t_PCSrc = '1' and t_RegWrite = '1' and t_MemWrite = '1' and t_CarryFlag = '1' and t_InterruptControlWrite = '1' and t_MCycleStart = '1') report "Failed CondLogic Test Case 7.1" severity error;
         wait for ClkPeriod * 4 / 10; -- 1 / 10 ClkPeriods after edge.
         -- Flags should be false, so EQ will be false (Z != 1).
-        assert (t_PCSrc = '0' and t_RegWrite = '0' and t_MemWrite = '0' and t_CarryFlag = '0') report "Failed CondLogic Test Case 7.2" severity error;
+        assert (t_PCSrc = '0' and t_RegWrite = '0' and t_MemWrite = '0' and t_CarryFlag = '0' and t_InterruptControlWrite = '0' and t_MCycleStart = '0') report "Failed CondLogic Test Case 7.2" severity error;
 
         wait for ClkPeriod * 4 / 10;
 
@@ -159,9 +159,9 @@ begin
         t_FinalFlags <= "1100"; t_FlagW <= "011";
         wait for ClkPeriod;
         -- Flags should still be false, so NEQ will be true (Z == 0).
-        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_Cond <= "0001";
+        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_MCycleS <= '1'; t_InterruptControlW <= '1'; t_Cond <= "0001";
         wait for ClkPeriod / 10;
-        assert (t_PCSrc = '1' and t_RegWrite = '1' and t_MemWrite = '1' and t_CarryFlag = '0') report "Failed CondLogic Test Case 8" severity error;
+        assert (t_PCSrc = '1' and t_RegWrite = '1' and t_MemWrite = '1' and t_CarryFlag = '0' and t_InterruptControlWrite = '1' and t_MCycleStart = '1') report "Failed CondLogic Test Case 8" severity error;
 
         wait for ClkPeriod * 9 / 10;
 
@@ -169,9 +169,9 @@ begin
         t_FinalFlags <= "0011"; t_FlagW <= "100";
         wait for ClkPeriod;
         -- Flags should still be false, so VC (no overflow) will be true (V == 0).
-        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_Cond <= "0111";
+        t_PCS <= '1'; t_RegW <= '1'; t_MemW <= '1'; t_MCycleS <= '1'; t_InterruptControlW <= '1'; t_Cond <= "0111";
         wait for ClkPeriod / 10;
-        assert (t_PCSrc = '1' and t_RegWrite = '1' and t_MemWrite = '1' and t_CarryFlag = '0') report "Failed CondLogic Test Case 9" severity error;
+        assert (t_PCSrc = '1' and t_RegWrite = '1' and t_MemWrite = '1' and t_CarryFlag = '0' and t_InterruptControlWrite = '1' and t_MCycleStart = '1') report "Failed CondLogic Test Case 9" severity error;
 
         wait for ClkPeriod * 9 / 10;
 
